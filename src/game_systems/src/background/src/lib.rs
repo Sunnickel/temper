19f640a1720a2f8b608e8723755937cc64811056
Tonguechaude <evan.challias@tonguechaude.fr>
2026-03-27 20:56:22 +0100

    fix : remove duplicate particle system

--- src/game_systems/src/background/src/lib.rs
+++ src/game_systems/src/background/src/lib.rs
@@ -11,7 +11,6 @@ pub mod keep_alive_system;
 pub mod lan_pinger;
 pub mod mq;
 pub mod send_entity_updates;
-pub mod send_particles;
 pub mod server_command;
 pub mod world_sync;
 
@@ -21,6 +20,5 @@ pub fn register_background_systems(schedule: &mut bevy_ecs::prelude::Schedule) {
     schedule.add_systems(day_cycle::tick_daylight_cycle);
     schedule.add_systems(mq::process);
     schedule.add_systems(send_entity_updates::handle);
-    schedule.add_systems(send_particles::handle);
     schedule.add_systems(server_command::handle);
 }
